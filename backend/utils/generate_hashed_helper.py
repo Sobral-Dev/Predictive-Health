8f1bcf1286d14f538078e84c4304104a86a4d15f
Felipe Sobral <felipesobral_@hotmail.com>
2025-03-17 18:58:08 -0300

    fix vulnerabilities

--- backend/utils/generate_hashed_helper.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-# Gere uma senha hasheada
-password = "853211"
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-print(hashed_password.decode('utf-8'))
--- frontend/globalSetup.ts
+++
@@ -1,9 +0,0 @@
-import { beforeAll, afterAll } from 'vitest';
-
-beforeAll(() => {
-  console.log('Configurações globais iniciais realizadas.');
-});
-
-afterAll(() => {
-  console.log('Limpeza global realizada.');
-});
